eea8e8e88ab46ea1b0cd5a72acd89a50065f5f28
Aadith_Th <taadith@gmail.com>
2023-07-26 23:53:03 -0500

    ocr testing is a work in progress

--- tesseract-ocr-testing.py
+++ tesseract-ocr-testing.py
@@ -1,2 +1,29 @@
 import os
-os.system("echo Hello from the other side!")
+
+def get_file_name(file_path):
+    file_name, file_extension = os.path.splitext(os.path.basename(file_path))
+    return file_name
+
+def get_files_from_path(file_path):
+    #check if it is a file or folder path
+    if not os.path.isdir(file_path):
+        return [file_path]
+    else:
+        # get a list of all files in the folder
+        contents = os.listdir(file_path)
+        files = [file for file in contents if not os.path.isdir(file)]
+        return files
+
+def generate_ocr_text(file_path,output_path = "./out"):
+    pass
+
+print("Test (1)")
+print(get_file_name("./tamil-text/example1.jpeg"))
+print(get_file_name("./tamil-text/tamil.png"))
+print(get_file_name("./tamil-text"))
+print()
+
+print("Test (2)")
+print(get_files_from_path("./tamil-text"))
+print(get_files_from_path("./tesseract-ocr-testing.py"))
+print(get_files_from_path("."))
