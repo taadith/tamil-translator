7b91ff3174959c88c8501861c132486b61fbe572
Aadith_Th <taadith@gmail.com>
2023-07-27 23:23:26 -0500

    dealing with bug of running tesseract command thru os.system(...)

--- tesseract-ocr-testing.py
+++ tesseract-ocr-testing.py
@@ -7,8 +7,6 @@ def create_directory(folder_path):
     try:
         os.mkdir(folder_path)
         print(f"Directory '{folder_path}' created successfully.")
-    except FileExistsError:
-        print(f"Directory '{folder_path}' already exists.")
     except Exception as e:
         print(f"Error while creating directory: {str(e)}")
 
@@ -19,21 +17,34 @@ def get_file_name(file_path):
 def get_files_from_path(file_path):
     #check if it is a file or folder path
     if not os.path.isdir(file_path):
-        return [file_path]
+        return [os.path.abspath(file_path)]
     else:
         # get a list of all files in the folder
         contents = os.listdir(file_path)
-        files = [file for file in contents if not os.path.isdir(file)]
+        files = [os.path.abspath(file) for file in contents if not os.path.isdir(file)]
         return files
 
 def generate_ocr_text(file_path,output_path = "./out"):
+    print("Checking if directory exists...")
     if not directory_exists(output_path):
+        print("Directory doesn't exist...")
         create_directory(output_path)
+    else:
+        print("Directory exists!")
+    
+    print("Grabbing all files from the file path...")
     files = get_files_from_path(file_path)
-    for file in files:
-        output_txt_filename = output_path + "/" + get_file_name(file)
-        shell_command = "tesseract " + file_path + " " + output_txt_filename + " -l tam"
-        os.system(shell_command)
+
+    //TODO: fix this portion of code
+
+    # print("Iterating thru each file...")
+    # for file in files:
+    #     print("Working on " + file + "...")
+    #     output_txt_filename = output_path + "/" + get_file_name(file)
+    #     shell_command = "tesseract " + file + " " + output_txt_filename + " -l tam"
+    #     os.system(shell_command)
+    #     print("Completed working on file: " + file)
+    # print("Task completed")
 
 # print("Test (1)")
 # print(get_file_name("./tamil-text/example1.jpeg"))
@@ -46,4 +57,4 @@ def generate_ocr_text(file_path,output_path = "./out"):
 # print(get_files_from_path("./tesseract-ocr-testing.py"))
 # print(get_files_from_path("."))
 
-generate_ocr_text("./tamil-text/example1.jpeg")
+generate_ocr_text("./tamil-text")
